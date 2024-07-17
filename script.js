11d3d590c59f2131f55b4b901bc43bace33ae697
notsag <gastonmaid2018@gmail.com>
2024-07-17 13:15:32 -0300

    IIBB agregado (test)

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ function calcularImpuestoUSD() {
         .then(response => response.json())
         .then(datos => {
             const dolarVenta = datos.venta; /* tomo el valor del dolar venta desde la api */
+            const provinciaIIBB = document.getElementById('provinciaUSD').value; /* Tomo el valor del porcentaje de IIBB de la provincia seleccionada */
             const precioUSD = document.getElementById('precioUSD').value; /* tomo el valor de dolar dado en el input del html */
 
             if (precioUSD) {
-                const precioEnARS = precioUSD * dolarVenta * 1.10; /* Los bancos suelen tomar la cotiz un +10% más por eso agregar un *1.10 */
-                const impuesto = precioEnARS * 0.59;
-                const precioTotal = precioEnARS + impuesto;
+                const precioARS = precioUSD * dolarVenta * 1.10; /* Los bancos suelen tomar la cotiz un +10% más por eso agregar un *1.10 */
+                const impuesto = precioARS * 0.59;
+                const impuestoIIBB = precioARS * provinciaIIBB
+                const precioTotal = precioARS + impuesto + impuestoIIBB;
 
                 document.getElementById('resultadoUSD').innerText = `El precio total en ARS con impuestos es: ${precioTotal.toFixed(2)} ARS`;
             } else {
@@ -20,9 +22,11 @@ function calcularImpuestoUSD() {
 
 function calcularImpuestoARS(){
         const precioARS = document.getElementById('precioARS').value; /* tomo el valor de dolar dado en el input del html */
+        const provinciaIIBB = document.getElementById('provinciaARS').value; /* Tomo el valor del porcentaje de IIBB de la provincia seleccionada */
 
             if (precioARS) {
-                const precioTotal = precioARS * 1.59;
+                const impuestoIIBB = precioARS * provinciaIIBB
+                const precioTotal = precioARS * 1.59 + impuestoIIBB;
                 document.getElementById('resultadoARS').innerText = `El precio total en ARS con impuestos es: ${precioTotal.toFixed(2)} ARS`;
             } else {
                 document.getElementById('resultadoARS').innerText = 'Por favor, ingrese un precio en ARS.';
